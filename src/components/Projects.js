be96f469be9416d57cbcb117a456e23c7295e82e
moetez96 <moetez.ayari@esprit.tn>
2021-12-30 20:53:02 +0100

    complete portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
 import ProjectContainer from "./ProjectContainer";
 import { Element } from "react-scroll";
 
